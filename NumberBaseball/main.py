2a379000da908c36f441fa9220ac9f9a17ebfce1
SeoooooNyeong <113419106+SeoooooNyeong@users.noreply.github.com>
2024-08-30 15:21:59 +0900

    Update main.py
    Author : sunyoung
    Date : 2024/08/30 15:21
    
    숫자 야구게임 완성
    answer은 random 함수를 통해 값을 받아옴
    이중 for문을 통해 답 확인

--- NumberBaseball/main.py
+++ NumberBaseball/main.py
@@ -1,12 +1,20 @@
+import random
+
 count = 0        #반복횟수
-answer = [0,1,2] #정답의 각 자릿수
-sbCount = [0,0]  #Strike와 Ball의 개수를 세는 리스트
+#answer = [0,1,2] #정답의 각 자릿수
+answer = random.sample(range(1, 10), 3)
 
-while(count < 3):
-    try:
-        a, b, c = input("3자리 숫자를 입력해주세요: ")
-        break
-    except:
-        count += 1
+while(count<3):
+    sbCount = [0,0]  #Strike와 Ball의 개수를 세는 리스트
+    game_input = list(map(int, input("3자리 숫자를 입력해주세요: ")))
+    for i in range(3):
+        for j in range(3):
+            if(answer[i] == game_input[j]):
+                if(i==j):
+                    sbCount[0] += 1
+                else:
+                    sbCount[1] += 1
+    print(f"{sbCount[0]}S {sbCount[1]}B")
+    count += 1
 
-
+print(answer)
